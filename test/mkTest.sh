01fdaacd1d845caa6a2385ccd23a1422e1abad72
dfischer <>
2005-06-21 16:43:09 +0000

    release 0.2.6.2

--- test/mkTest.sh
+++ test/mkTest.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 mtasc -cp /usr/local/share/mtasc/std -swf classes.swf -separate -header 320:240:25 Main.as org/swfmill/Foo.as
-swfmill simple test.swfml output.swf
+../src/swfmill simple test.swfml output.swf
 #&& mozilla -chrome chrome://fludge/content/fludge.xul?$(pwd)/output.swf 
