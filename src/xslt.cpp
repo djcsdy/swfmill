2687537c91906b75c95d7130392c0de3ad8eb7bc
dfischer <>
2005-07-19 10:27:33 +0000

    moved swft/xslt to its own subdirs

--- src/xslt.cpp
+++
@@ -1,6 +0,0 @@
-#include "xslt.h"
-
-const char *swf_internal_stylesheets[6] = {
-		"simple2lowlevel.xsl", xslt_simple2lowlevel,
-		"svg2lowlevel.xsl", xslt_svg2lowlevel,
-		NULL, NULL };
--- src/xslt/xslt.h
+++ src/xslt/xslt.h
@@ -1,12 +1,8 @@
 #ifndef SWF_XSLT_H
 #define SWF_XSLT_H
 
-#include <libxml/tree.h>
-
 // definitions for "in-memory" stylesheets
 extern const char *xslt_simple2lowlevel;
 extern const char *xslt_svg2lowlevel;
-
-extern const char **swf_internal_stylesheets;
 
 #endif
