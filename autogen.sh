909d3547fe2f6db45c270b7f7f61e927d15fffa7
Daniel Cassidy <mail@danielcassidy.me.uk>
2009-12-26 14:58:17 +0000

    autogen.sh: prefer glibtoolize to libtoolize if available (compatibility with OSX).

--- autogen.sh
+++ autogen.sh
@@ -1,7 +1,13 @@
 #!/bin/sh
 
+if which glibtoolize; then
+	LIBTOOLIZE=glibtoolize
+else
+	LIBTOOLIZE=libtoolize
+fi
+
 set -x
 aclocal -I autoconfig/m4
-libtoolize --force --copy
+$LIBTOOLIZE --force --copy
 automake --foreign --add-missing --copy
 autoconf
