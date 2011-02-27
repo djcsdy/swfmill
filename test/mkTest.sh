6fc6764ec7d3581366c75e48f1eff7f309fc1db7
piersh@hotmail.com <>
2011-02-27 00:11:15 -0800

    cleanup test build/run

--- test/mkTest.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-mtasc -cp /usr/local/share/mtasc/std -swf classes.swf -header 320:240:25 Main.as org/swfmill/Foo.as
-../src/swfmill -v simple test.swfml output.swf
-#&& mozilla -chrome chrome://fludge/content/fludge.xul?$(pwd)/output.swf 
