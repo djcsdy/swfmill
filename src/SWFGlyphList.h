96cbbd47b5446fa8ac4f32c0869022e82ed953f3
Daniel Cassidy <mail@danielcassidy.me.uk>
2012-01-21 17:40:25 +0000

    Reformat SWFGlyphList.cpp and .h.

--- src/SWFGlyphList.h
+++ src/SWFGlyphList.h
@@ -5,29 +5,29 @@
 
 namespace SWF {
 
-class GlyphShape;
-	
-class GlyphList : public Item {
-	/* glyph lists have strange offsetting stuff, so we do them manually */
-	public:
-		GlyphList();
-		virtual ~GlyphList();
-		virtual bool parse( Reader *r, int end, Context *ctx );
-		virtual void dump( int indent, Context *ctx );
-		virtual size_t calcSize( Context *ctx, int start_at );
-		virtual void write( Writer *w, Context *ctx );
-		virtual void writeXML( xmlNodePtr node, Context *ctx );
-		virtual void parseXML( xmlNodePtr node, Context *ctx );
-	
-		void allocate( int n );
-		GlyphShape *getShapeN( int n );
-		void setMapN( int n, int m );
-	
-	protected:
-		int nGlyphs;
-		GlyphShape *glyphs;
-		int *map;
-};
+	class GlyphShape;
+
+	class GlyphList : public Item {
+		/* glyph lists have strange offsetting stuff, so we do them manually */
+		public:
+			GlyphList();
+			virtual ~GlyphList();
+			virtual bool parse(Reader *r, int end, Context *ctx);
+			virtual void dump(int indent, Context *ctx);
+			virtual size_t calcSize(Context *ctx, int start_at);
+			virtual void write(Writer *w, Context *ctx);
+			virtual void writeXML(xmlNodePtr node, Context *ctx);
+			virtual void parseXML(xmlNodePtr node, Context *ctx);
+
+			void allocate(int n);
+			GlyphShape *getShapeN(int n);
+			void setMapN(int n, int m);
+
+		protected:
+			int nGlyphs;
+			GlyphShape *glyphs;
+			int *map;
+	};
 
 }
 
