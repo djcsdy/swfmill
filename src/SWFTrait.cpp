35a5b0e9ec71f5c63faf70ec98a6af2d64ff5a6d
Daniel Cassidy <mail@danielcassidy.me.uk>
2013-11-01 19:21:24 +0000

    Reformat SWFTrait.*.

--- src/SWFTrait.cpp
+++ src/SWFTrait.cpp
@@ -4,32 +4,32 @@
 
 namespace SWF {
 
-Trait *Trait::get( Reader *r, int end, Context *ctx ) {
-	int type = r->getNBitInt( 4 );
-	int len = end - r->getPosition();
-	
-	Trait* ret = getByType( type );
+	Trait *Trait::get(Reader *r, int end, Context *ctx) {
+		int type = r->getNBitInt(4);
+		int len = end - r->getPosition();
 
-	if( !ret ) {
-		ret = handleError( type );
+		Trait* ret = getByType(type);
+
+		if (!ret) {
+			ret = handleError(type);
+		}
+
+		if (ret) {
+			ret->setType(type);
+			ret->setLength(len);
+			ret->parse(r, end, ctx);
+		}
+
+		return ret;
 	}
 
-	if( ret ) {
-		ret->setType( type );
-		ret->setLength( len );
-		ret->parse( r, end, ctx );
-	} 
+	void Trait::writeHeader(Writer *w, Context *ctx, size_t len) {
+		w->putNBitInt(type, 4);
+	}
 
-	return ret;
-}	
-
-void Trait::writeHeader( Writer *w, Context *ctx, size_t len ) {
-	w->putNBitInt( type, 4 );
-}
-
-Trait* Trait::handleError( int type ) {
-	fprintf(stderr,"WARNING: unknown trait type 0x%02X\n", type );
-	return NULL;
-}
+	Trait* Trait::handleError(int type) {
+		fprintf(stderr,"WARNING: unknown trait type 0x%02X\n", type);
+		return NULL;
+	}
 
 }
